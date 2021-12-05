fc0e76c6a675604ca47fb61667e52ba96e51cee6
kutluhanozbakan <kutluhanozbakan21@gmail.com>
2021-12-05 18:28:59 +0300

    asda

--- note_me/lib/screens/homepage.dart
+++ note_me/lib/screens/homepage.dart
@@ -1,39 +1,80 @@
-// ignore_for_file: prefer_const_constructors
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:note_me/model/user_model.dart';
 import 'package:note_me/screens/login.dart';
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  _HomeState createState() => _HomeState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+class _HomeScreenState extends State<HomeScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
 
-class _HomeState extends State<Home> {
-  GoogleSignInAccount? user = _googleSignIn.currentUser;
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          actions: [
-            ElevatedButton(
-              child: const Text("Sign out"),
-              onPressed: user != null
-                  ? null
-                  : () async {
-                      await _googleSignIn.signOut();
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance.collection("users").doc(user!.uid).get().then((value) {
+      this.loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
 
-                      Navigator.pop(context);
-                    },
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height / 3.5,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.orange,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 10, right: MediaQuery.of(context).size.width / 2),
+                  child: Text(
+                    "Hoşgeldin",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-        body: Center(
-          child: Text("hi"),
-        ),
-      );
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text("${loggedInUser.firstName} ${loggedInUser.secondName}",
+              style: TextStyle(
+                color: Colors.black54,
+                fontWeight: FontWeight.w500,
+              )),
+          Text("${loggedInUser.email}",
+              style: TextStyle(
+                color: Colors.black54,
+                fontWeight: FontWeight.w500,
+              )),
+          SizedBox(
+            height: 15,
+          ),
+          ActionChip(
+              label: Text("Logout"),
+              onPressed: () {
+                logout(context);
+              }),
+        ],
+      ),
+    );
+  }
+
+  // the logout function
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
 }
